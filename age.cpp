21f316aa28d91b5d22d4c1b2496f6160dcbd7d86
PragatiAsnani <asnanipragati@gmail.com>
2026-02-26 00:21:48 +0530

    age eligibility

--- age.cpp
+++ age.cpp
@@ -3,16 +3,16 @@ using namespace std;
 int main(){
     int age;
     cin >> age ;
-    if age < 18 {
+    if (age < 18) {
         cout << "you are not eligible";
     }
     if (age > 18 && age <= 54){
         cout << "you are eligible";
     }
     if (age > 54 && age <= 57){
-        cout << "you are eligible but retirement soon!"
+        cout << "you are eligible but retirement soon!" ;
     }
-    if age > 57{
+    if (age > 57) {
         cout << "retirement time" ;
     }
     return 0 ;
