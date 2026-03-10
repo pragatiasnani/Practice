508917b3a8f73ad128c30c6cbd49f481b30dc57c
PragatiAsnani <asnanipragati@gmail.com>
2026-03-11 02:08:42 +0530

    fixed operator validation and division by zero'

--- calculator.cpp
+++ calculator.cpp
@@ -1,42 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std ;
 int main (){
-    double num1 ;
-    double num2 ;
+
+    double num1, num2 ;
     char op ;
     
     cout << "**************CALCULATOR**************" << endl ;
 
-    cout << "enter your operator (+, =, / , *): " ;
+    cout << "enter your operator (+, -, / , *): " ;
     cin >> op ;
 
-    cout << "enter number 1: " ;
-    cin >> num1 ;
-
-    cout << "enter number 2: " ;
-    cin >> num2 ;
-
-    switch (op) {
-        case '+' :
-        cout << "sum of the 2 numbers is: " << num1 + num2 << endl ;
-        break ;
-
-        case '-' :
-        cout <<"difference of the 2 numbers is: " << num1 - num2 << endl ;
-        break ;
-
-        case '*' :
-        cout << "multiplication of the 2 numbers is: " << num1 * num2 << endl ;
-        break ;
-
-        case '/' :
-        cout << "division of the 2 numbers is: " << num1 / num2 << endl ;
-        break ;
-
-        default :
-        cout << "use operators amongst +, -, *, / only" << endl ;
-        break;
+    if (op != '+' && op != '-' && op != '*' && op!= '/') {
+        cout << "use operators amongst +, -, *, / only" << endl;
     }
+    
+    else {
+        cout << "enter number 1: " ;
+        cin >> num1 ;
+
+        cout << "enter number 2: " ;
+        cin >> num2 ;
+
+        if (op == '/' && num2 == 0) {
+            cout << "division by zero is not allowed!" << endl ;
+        }
+        
+        else {
+            switch (op) { 
+                case '+' :
+                cout << "sum of the 2 numbers is: " << num1 + num2 << endl ;
+                break ;
+
+                case '-' :
+                cout <<"difference of the 2 numbers is: " << num1 - num2 << endl ;
+                break ;
+
+                case '*' :
+                cout << "multiplication of the 2 numbers is: " << num1 * num2 << endl ;
+                break ;
+
+                case '/' :
+                cout << "division of the 2 numbers is: " << num1 / num2 << endl ;
+                break ;
+
+            }
+        }
+    }
+    
     cout << "**************************************" << endl ;
 
     return 0 ;
